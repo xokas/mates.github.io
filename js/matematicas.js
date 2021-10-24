6dcb83e6e366083cfb26e3e0fba29b7bbe656747
Joaquin Lousa <joaquin.lousa@gmail.com>
2021-10-24 14:41:56 +0200

    mejoras en las sumas

--- js/matematicas.js
+++ js/matematicas.js
@@ -1,24 +1,22 @@
 let gLevel = 2;
-let gResult = [];
 
 $(document).ready(function(){
 	console.log("matemacias begin!");
 	//document.querySelectorAll('[contenteditable=true]').get(0).focus();
 	$(document).contents().find("div[contenteditable='true']").first().focus();
 	for(let i = 1;i < 5;i++){
-		gResult[i] = 0;
 		suma(i);
 	}
 });
 
 function suma(i){
+	let id = "#suma" + i;
 	let sumando1 = 0;
 	let sumando2 = 0;
 	let diff = Math.pow(10, gLevel);
 	let limitInf = diff / 10;
 	console.log(diff + " " + limitInf);
-	clean(i);
-	let id = "#suma" + i;
+	clean(id);
 	
 	do{
 		sumando1 = Math.floor(Math.random() * diff);
@@ -30,39 +28,35 @@ function suma(i){
 	$(id + " [name='sumando1']").text(sumando1);
 	$(id + " [name='sumando2']").text(sumando2);
 	$(id + " [name='result']").text("");
-
-	gResult[i] = sumando1 + sumando2;
 }
 
 function validar(i){
 	let id = "#suma" + i;
-	console.log(gResult[i]);
-	console.log(parseInt($(id + " [name='result']").html()));
-	if (gResult[i] == parseInt($(id + " [name='result']").html())) {
-		success(i);
+	let sumando1 = parseInt($(id + " [name='sumando1']").html());
+	let sumando2 = parseInt($(id + " [name='sumando2']").html())
+	let result = parseInt($(id + " [name='result']").html())
+	if ((sumando1 + sumando2) == result) {
+		success(id);
 	} else {
-		error(i);
+		error(id);
 	}
 }
 
-function clean(i){
-	let id = "#suma" + i;
+function clean(id){
 	$(id + " [name='result']").parent().removeClass("bg-success");
 	$(id + " [name='result']").parent().removeClass("bg-danger");
 	$(id + " [name='success']").addClass("d-none");
 	$(id + " [name='error']").addClass("d-none");
 }
 
-function error(i){
-	let id = "#suma" + i;
+function error(id){
 	$(id + " [name='result']").parent().removeClass("bg-success");
 	$(id + " [name='result']").parent().addClass("bg-danger");
 	$(id + " [name='success']").addClass("d-none");
 	$(id + " [name='error']").removeClass("d-none");
 }
 
-function success(i){
-	let id = "#suma" + i;
+function success(id){
 	$(id + " [name='result']").parent().removeClass("bg-danger");
 	$(id + " [name='result']").parent().addClass("bg-success");
 	$(id + " [name='success']").removeClass("d-none");
